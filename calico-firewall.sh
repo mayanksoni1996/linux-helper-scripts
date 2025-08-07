c455595cee5e14dfc4c4cefa6777434160382ee7
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2025-08-06 19:36:42 -0700

    Update calico-firewall.sh

--- calico-firewall.sh
+++ calico-firewall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo firewall-cmd --permanent --new-zone=calico-trusted
 sudo firewall-cmd --permanent --zone=calico-trusted --set-target=ACCEPT
 sudo firewall-cmd --permanent --zone=calico-trusted --add-interface=vxlan.calico
